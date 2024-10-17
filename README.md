4bc8dbb5ccf3d9375107f6dcd95ba1a6990e8841
Sinethemba7vitsha <vitsha709@gmail.com>
2024-10-17 08:20:11 +0200

    Third Commit

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# Capaciti
+# Capaciti Portfolio
+
+## Table of Contents
+- [Technologies Used](#technologies-used)
+- [Features](#features)
+- [Contact](#contact)
+
+
+## Technologies Used
+- HTML
+- CSS
+
+
+## Installation
+To view my portfolio locally, follow these steps:
+1. Clone the repository: `git clone https://github.com/Sinethemba7vitsha/portfolio.git`
+2. Navigate to the project directory: `cd portfolio`
+3. Open `index.html` in your browser.
+
+
+## Features
+- Showcase of projects with descriptions and links
+- Contact form for inquiries
+- Responsive design for mobile and desktop
+
+
+## Contact
+Feel free to reach out:
+- Email: Maxwellsucksatcode@gmail.com
---
+++ index.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("downloadButton").addEventListener("click", function () {
+      var a = document.createElement("a");
+      a.href = "./CV.pdf";
+      a.download = "CV.pdf";
+      a.click();
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const text = "Hi. I’m Sinethemba Vitsha.\nA Full Stack Developer.";
+    const typewriterText = document.getElementById("typewriter-text");
+    let index = 0;
+
+    function typeWriter() {
+        if (index < text.length) {
+            if (text.charAt(index) === "\n") {
+                typewriterText.innerHTML += "<br>";
+            } else {
+                typewriterText.innerHTML += text.charAt(index);
+            }
+            index++;
+            setTimeout(typeWriter, 100); // Adjust speed here (lower value = faster typing)
+        }
+    }
+
+    typeWriter();
+});
